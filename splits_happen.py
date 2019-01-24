991498879a44140e9778a7e8786699d22c557887
Yemi Fawehinmi <oluyemi@gmail.com>
2019-01-23 21:05:57 -0500

    Add comments and very minor changes

--- splits_happen.py
+++ splits_happen.py
@@ -8,8 +8,7 @@
 
 
 def splits_happen(line):
-    score = 0
-
+    # Translate a symbol into a numeric value for scoring
     def convert_symbol_to_score(symbol):
         if symbol == 'X':
             return 10
@@ -20,12 +19,25 @@ def splits_happen(line):
         else:
             return 0
 
+    '''
+        Handle pairs in the case where a bowler makes 2 tries or calculating the scoring of a preceding
+        strike. A spare should ALWAY be the second value in the try and can never be the first
+    '''
+
     def calc_two_try_score(sub_line):
         if sub_line[0] and sub_line[1] == '/':
             return 10
         else:
             return convert_symbol_to_score(sub_line[0]) + convert_symbol_to_score(sub_line[1])
 
+    total_score = 0
+
+    '''
+        Base cases:
+            return a score of zero is nothing is passed
+            return the integer equivalent of input if a single string is passed
+            treat a two element string as a two try turn
+    '''
     if len(line) == 0:
         return 0
 
@@ -35,24 +47,28 @@ def splits_happen(line):
     if len(line) == 2:
         return calc_two_try_score(line)
 
+    '''
+        Recursively handle the scoring based on the first element
+    '''
+
     if len(line) > 2:
+
         if line[0] == 'X' and len(line[1:]) >= 2:
-            if len(line[1:]) == 2:
-                score = convert_symbol_to_score(line[0]) + calc_two_try_score(line[1:3])
+            if len(line[1:]) == 2:  # Tenth Frame
+                total_score = convert_symbol_to_score(line[0]) + calc_two_try_score(line[1:3])
             else:
-                score = convert_symbol_to_score(line[0]) + calc_two_try_score(line[1:3]) + splits_happen(line[1:])
+                total_score = convert_symbol_to_score(line[0]) + calc_two_try_score(line[1:3]) + splits_happen(line[1:])
 
-        if line[0] in "0123456789-":
-            score = convert_symbol_to_score(line[0]) + splits_happen(line[1:])
-
-        if line[0] in "0123456789" and line[1] == '/':
-            t1 = convert_symbol_to_score(line[0])
-            if len(line[2:]) == 1:
-                score = calc_two_try_score(line[0:2]) + convert_symbol_to_score(line[2])
+        if line[0] in "0123456789-" and line[1] == '/':
+            if len(line[2:]) == 1:  # Tenth Frame
+                total_score = calc_two_try_score(line[0:2]) + convert_symbol_to_score(line[2])
             else:
-                score = calc_two_try_score(line[0:2]) + convert_symbol_to_score(line[2]) + splits_happen(line[2:])
+                total_score = calc_two_try_score(line[0:2]) + convert_symbol_to_score(line[2]) + splits_happen(line[2:])
 
-    return score
+        if line[0] in "0123456789-" and line[1] != '/':
+            total_score = convert_symbol_to_score(line[0]) + splits_happen(line[1:])
+
+    return total_score
 
 
 if __name__ == '__main__':
